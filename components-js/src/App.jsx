c39f8002f1995c04ecdc07cf5ac03ed11ea5e7c1
PJ-21-code <pr.pranjal21@gmail.com>
2026-06-24 17:08:51 +0530

    Fix unclickable components-js folder

---
+++ components-js/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/card.jsx'
+import Navbar from './components/navbar.jsx'
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ components-js/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='card'>
+        <h1>Pranjal</h1>
+    </div>
+  )
+}
+
+export default card
